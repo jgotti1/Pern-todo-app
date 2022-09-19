290ce6e70026472cb4b249fe1a640d9eccac75e9
jgotti1 <96301204+jgotti1@users.noreply.github.com>
2022-09-19 17:48:41 -0400

    working deployed front and backend. hostgrid front AWS BS back

--- server/db.js
+++ server/db.js
@@ -1,13 +1,12 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-
 const dbPernToDo = new Pool({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
-  host: "127.0.0.1",
+  host: process.env.DB_HOST,
   dialect: "postgres",
 });
 
