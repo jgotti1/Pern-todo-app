3abfbf96ea003a549d5d0c99b59123e37b2e4991
John Margotti <jgotti1@optonline.net>
2022-10-04 09:17:35 -0400

    deploy changes

--- client/src/components/hooks/FetchPaths.js
+++ client/src/components/hooks/FetchPaths.js
@@ -1,8 +1,8 @@
 // ++++++++++++++++ DEV PATHS ++++++++++++++++
 
-// export const fetchPath = "http://localhost:5000/todos";
+export const fetchPath = "http://localhost:5000/todos";
 
 //+++++++++++++++++++PROD PATHS ++++++++++++++++++++
 
-// export const fetchPath = "https://johntodo.us-east-1.elasticbeanstalk.com/todos";
-export const fetchPath = "https://johntodo.johnmargotti.com/todos";
+// export const fetchPath = "https://marciestodo.johnmargotti.com/todos";
+// export const fetchPath = "https://johntodo.johnmargotti.com/todos";
