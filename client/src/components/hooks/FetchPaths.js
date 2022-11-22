5c779bb26d42d85981f079ce36f238f12db0ebd3
John Margotti <jgotti1@optonline.net>
2022-11-22 10:46:03 -0500

    changed ico file

--- client/src/components/hooks/FetchPaths.js
+++ client/src/components/hooks/FetchPaths.js
@@ -3,6 +3,10 @@
 // export const fetchPath = "http://localhost:5000/todos";
 
 // //+++++++++++++++++++PROD PATHS ++++++++++++++++++++
+//John Cyclic
 
-export const fetchPath = "https://marciestodo.johnmargotti.com/todos";
-// export const fetchPath = "https://johntodo.johnmargotti.com/todos";
+export const fetchPath = "https://zany-erin-tick-hem.cyclic.app/todos";
+
+//Marcie Cyclic host
+
+// export const fetchPath = "https://sleepy-sandals-seal.cyclic.app/todos";
